306e548c431b0fbc87ef294920d52a2e68d9c8ef
Giulia Di Liello <giulia.di.liello@accenture.com>
2022-06-29 21:15:25 +0200

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    getQuota:'https://finnhub.io/api/v1/quote',
+    search:'https://finnhub.io/api/v1/search',
+    sentiment:'https://finnhub.io/api/v1/stock/insider-sentiment',
+  }
 };
